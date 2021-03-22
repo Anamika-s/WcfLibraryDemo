754124194ea9162d420490aa680f7df600231088
Anamika <anamika.sawhney22@gmail.com>
2021-03-22 13:58:09 +0530

    Add project files.

---
+++ CallingService1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CallingService1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client service1Client = new ServiceReference1.Service1Client();
+            Console.WriteLine(service1Client.GetData());
+            Console.Read();
+        }
+    }
+}
---
+++ ServiceCallin2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ServiceCallin2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ServiceReference1.Service2Client client = new ServiceReference1.Service2Client();
+            Console.WriteLine(client.AddEmployee());
+
+            Console.ReadLine();
+
+        }
+    }
+}
